70f3608de153ba490fb613c1839dbd814a9ddedc
shander3377 <agrim310108@gmail.com>
2023-09-25 18:25:58 +0530

    sxzX

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import { Button } from "@nextui-org/button";
 export default function Home() {
 	return (
