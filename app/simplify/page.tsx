fbe0f7434a8c03cb0871b3f89dcd58a74dbf59a5
shander3377 <agrim310108@gmail.com>
2023-09-25 18:27:52 +0530

    asdasdasdas

--- app/simplify/page.tsx
+++ app/simplify/page.tsx
@@ -6,7 +6,6 @@ import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import { Button } from "@nextui-org/button";
 import {
 	Modal,
